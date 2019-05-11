62d47628d4b0a7140fb67f62f89b5ca8646121b4
korvinius <korvinius@wmc>
2019-05-11 19:13:04 +0300

    wmct-v0.0.1

---
+++ contracts/wmct.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+
+
+contract WMCToken is StandardToken, Ownable  {
+
+	string public constant name = "WMC Token";
+	string public constant symbol = "WMCT";
+	uint32 public constant decimals = 8;
+    
+}
